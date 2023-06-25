bf18db3c2dee6c2422ba4ccb5ce8f21851a6925c
DiegoMolina65 <diegobrian042@gmail.com>
2023-06-24 21:00:34 -0400

    deleted:    mqtt.php 	new file:   mqtt_bme680.php 	new file:   mqtt_distance.php 	new file:   mqtt_mq135.php

---
+++ mqtt_distance.php
@@ -0,0 +1,50 @@
+<?php
+use Bluerhinos\phpMQTT;
+require("phpMQTT.php");
+
+$host = "34.171.0.123"; 
+$port = 1883;
+$username = "diegomolina"; 
+$password = "diegomolina"; 
+$mqtt = new phpMQTT($host, $port, "PHP MQTT Client"); 
+
+if(!$mqtt->connect(true, NULL, $username, $password)) {
+  exit(1);
+}
+
+$topic = 'distance';
+$mqtt->subscribe([$topic => ["qos" => 0, "function" => "procMsg"]], 0);
+
+while($mqtt->proc()){
+}
+
+$mqtt->close();
+
+function procMsg($topic, $msg){
+  echo "Msg Received: $msg\n";
+  $json = json_decode($msg, true);
+  
+  // Assumed to have $conn as mysqli connection
+  // Please setup your database connection
+  $conn = new mysqli('localhost', 'phpmyadmin', 'admin', 'sensor_data');
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $distance = $json['distance'];
+  $stmt = $conn->prepare("INSERT INTO sensor_values (distance) VALUES (?)");
+  $stmt->bind_param("d", $distance);
+  
+  if ($conn->error) {
+    die("SQL error: " . $conn->error);
+  }
+  
+  $stmt->execute();
+
+  echo "New record created successfully";
+
+  $stmt->close();
+  $conn->close();
+}
+?>
